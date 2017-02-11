23ccd5dadf5f0dfbc4638f7d924912393ae64685
dave <dsparrow27@gmail.com>
2017-02-11 19:39:18 +1100

    naming to support user config paths, useful for per tool setup or studio

--- zoo/libs/naming/naming.py
+++ zoo/libs/naming/naming.py
@@ -8,8 +8,11 @@ class NameManager(object):
     naming convention, we use the terms 'rule' and 'tokens' throughout the class to describe the logic.
     rules are just a basic expression like so '{side}_{area}_{counter}_{type}', the '_' isnt not necessary for any logic.
     the characters within the curly brackets are tokens which will be replace when its resolved.
+    tokens have a set of possible values that it can use if the value set this token to doesn't exist then it wont be
+    resolved. you can add tokens and values in memory per instance or you can add it to the config JSON file.
     """
     BASE_CONFIG_VAR = "ZOO_NAMING_BASE_PATH"
+    USER_CONFIG_VAR = "ZOO_NAMING_USER_PATHS"
     # super temp
     os.environ[BASE_CONFIG_VAR] = os.path.realpath(os.path.join(os.path.dirname(__file__), "config.json"))
 
@@ -97,7 +100,7 @@ class NameManager(object):
             if token == "counter":
                 val = str(self.counter["value"]).zfill(self.counter["padding"])
             else:
-                val = self.tokenValue(token)
+                val = self.config["tokens"][token][self.tokenValue(token)]
             newStr = re.sub("{" + token + "}", val, newStr)
         return newStr
 
@@ -108,4 +111,16 @@ class NameManager(object):
     def load(self):
         configPath = os.environ[NameManager.BASE_CONFIG_VAR]
         configPath = file.loadJson(configPath)
-        self.config = configPath
+        data = configPath
+        if NameManager.USER_CONFIG_VAR in os.environ:
+            for p in os.environ[NameManager.USER_CONFIG_VAR].split(os.path.sep):
+                if not p or not os.path.exists(p):
+                    continue
+                userData = file.loadJson(p)
+                rules = userData.get("rules")
+                tokens = userData.get("tokens")
+                if rules:
+                    data["rules"].update(rules)
+                if tokens:
+                    data["tokens"].update(tokens)
+        self.config = data
