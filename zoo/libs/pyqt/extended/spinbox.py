30ee8e9d45b818efebda0beae64240c2e756bfb5
David Sparrow <dsparrow27@gmail.com>
2018-06-22 18:25:24 +1000

    replace extended spinboxes event filter with standard methods

--- zoo/libs/pyqt/extended/spinbox.py
+++ zoo/libs/pyqt/extended/spinbox.py
@@ -1,36 +1,19 @@
 from qt import QtCore
 from qt import QtWidgets
 
+DRAG_NONE = 0
+DRAG_HORIZONTAL = 1
+DRAG_VERTICAL = 2
+
+CURSOR_NONE = 0
+CURSOR_BLANK = 1
+CURSOR_ARROWS = 2
+
 
 class DragSpinBox(QtWidgets.QSpinBox):
     def __init__(self, *args, **kwargs):
         super(DragSpinBox, self).__init__(*args, **kwargs)
-
-        self.spinner = _SpinBoxDragFilter(self)
-        self.spinner.defaultValue = 0.0
-        self.installEventFilter(self.spinner)
-
-
-class DragDoubleSpinBox(QtWidgets.QDoubleSpinBox):
-    def __init__(self, *args, **kwargs):
-        super(DragDoubleSpinBox, self).__init__(*args, **kwargs)
-
-        self.spinner = _SpinBoxDragFilter(self)
-        self.spinner.defaultValue = 0.0
-        self.installEventFilter(self.spinner)
-
-
-class _SpinBoxDragFilter(QtCore.QObject):
-    DRAG_NONE = 0
-    DRAG_HORIZONTAL = 1
-    DRAG_VERTICAL = 2
-
-    CURSOR_NONE = 0
-    CURSOR_BLANK = 1
-    CURSOR_ARROWS = 2
-
-    def __init__(self, *args, **kwargs):
-        super(_SpinBoxDragFilter, self).__init__(*args, **kwargs)
+        self.defaultValue = 0.0
         self._dragSensitivity = 5  # pixels per step
         self._startSensitivity = 10  # pixel move to start the dragging
         # private vars
@@ -38,52 +21,95 @@ class _SpinBoxDragFilter(QtCore.QObject):
         self._leftover = 0
         self._dragStart = None
         self._firstDrag = False
-        self._dragType = self.DRAG_NONE
+        self._dragType = DRAG_NONE
 
-    def eventFilter(self, cls, e):
-        if e.type() == QtCore.QEvent.MouseButtonPress:
-            if e.button() & QtCore.Qt.RightButton:
-                cls.setValue(self.defaultValue)
-                return True
+    def mousePressEvent(self, event):
+        if event.button() & QtCore.Qt.RightButton:
+            self.setValue(self.defaultValue)
+            return True
 
-        elif e.type() == QtCore.QEvent.ContextMenu:
-            return True  # Kill the context menu
-
-        elif e.type() == QtCore.QEvent.MouseMove:
-            stepHolder = cls.singleStep()
-            print self._dragType
-            if self._dragType:
-                if self._dragType == self.DRAG_HORIZONTAL:
-                    delta = e.pos().x() - self._lastPos.x()
-                else:
-                    delta = self._lastPos.y() - e.pos().y()
-
-                self._leftover += delta
-                self._lastPos = e.pos()
-
-                cls.stepBy(int(self._leftover / self._dragSensitivity))
-                self._leftover %= self._dragSensitivity
-
+    def mouseMoveEvent(self, event):
+        stepHolder = self.singleStep()
+        if self._dragType:
+            if self._dragType == DRAG_HORIZONTAL:
+                delta = event.pos().x() - self._lastPos.x()
             else:
-                if e.buttons() & QtCore.Qt.LeftButton:  # only allow left-click dragging
-                    if self._dragStart is None:
-                        self._dragStart = e.pos()
+                delta = self._lastPos.y() - event.pos().y()
 
-                    if abs(e.x() - self._dragStart.x()) > self._startSensitivity:
-                        self._dragType = self.DRAG_HORIZONTAL
-                    if abs(e.y() - self._dragStart.y()) > self._startSensitivity:
-                        self._dragType = self.DRAG_VERTICAL
+            self._leftover += delta
+            self._lastPos = event.pos()
 
-            cls.setSingleStep(stepHolder)
+            self.stepBy(int(self._leftover / self._dragSensitivity))
+            self._leftover %= self._dragSensitivity
+
+        else:
+            if event.buttons() & QtCore.Qt.LeftButton:  # only allow left-click dragging
+                if self._dragStart is None:
+                    self._dragStart = event.pos()
+
+                if abs(event.x() - self._dragStart.x()) > self._startSensitivity:
+                    self._dragType = DRAG_HORIZONTAL
+                elif abs(event.y() - self._dragStart.y()) > self._startSensitivity:
+                    self._dragType = DRAG_VERTICAL
+
+        self.setSingleStep(stepHolder)
+
+    def mouseReleaseEvent(self, event):
+        if self._firstDrag:
+            self._firstDrag = False
+        elif self._dragType:
+            self._dragType = DRAG_NONE
+            self._lastPos = QtCore.QPoint()
+            self._dragStart = None
 
 
-        elif e.type() == QtCore.QEvent.MouseButtonRelease:
-            # Only reset the dragType if it's *not* the first drag event release
-            if self._firstDrag:
-                self._firstDrag = False
-            elif self._dragType:
-                self._dragType = self.DRAG_NONE
-                self._lastPos = QtCore.QPoint()
-                self._dragStart = None
+class DragDoubleSpinBox(QtWidgets.QDoubleSpinBox):
+    def __init__(self, *args, **kwargs):
+        super(DragDoubleSpinBox, self).__init__(*args, **kwargs)
+        self.defaultValue = 0.0
+        self._dragSensitivity = 5  # pixels per step
+        self._startSensitivity = 10  # pixel move to start the dragging
+        # private vars
+        self._lastPos = QtCore.QPoint()
+        self._leftover = 0
+        self._dragStart = None
+        self._firstDrag = False
+        self._dragType = DRAG_NONE
+    def mousePressEvent(self, event):
+        if event.button() & QtCore.Qt.RightButton:
+            self.setValue(self.defaultValue)
+            return True
 
-        return cls.eventFilter(cls, e)
+    def mouseMoveEvent(self, event):
+        stepHolder = self.singleStep()
+        if self._dragType:
+            if self._dragType == DRAG_HORIZONTAL:
+                delta = event.pos().x() - self._lastPos.x()
+            else:
+                delta = self._lastPos.y() - event.pos().y()
+
+            self._leftover += delta
+            self._lastPos = event.pos()
+
+            self.stepBy(int(self._leftover / self._dragSensitivity))
+            self._leftover %= self._dragSensitivity
+
+        else:
+            if event.buttons() & QtCore.Qt.LeftButton:  # only allow left-click dragging
+                if self._dragStart is None:
+                    self._dragStart = event.pos()
+
+                if abs(event.x() - self._dragStart.x()) > self._startSensitivity:
+                    self._dragType = DRAG_HORIZONTAL
+                elif abs(event.y() - self._dragStart.y()) > self._startSensitivity:
+                    self._dragType = DRAG_VERTICAL
+
+        self.setSingleStep(stepHolder)
+
+    def mouseReleaseEvent(self, event):
+        if self._firstDrag:
+            self._firstDrag = False
+        elif self._dragType:
+            self._dragType = DRAG_NONE
+            self._lastPos = QtCore.QPoint()
+            self._dragStart = None
