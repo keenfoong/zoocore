9e2c933041819de3299096903bab3f2ec36815d9
Keen <keenfoong@gmail.com>
2018-07-31 14:02:09 +1000

    Documentation

--- zoo/libs/pyqt/widgets/roundbutton.py
+++ zoo/libs/pyqt/widgets/roundbutton.py
@@ -1,13 +1,30 @@
 from qt import QtCore, QtGui, QtWidgets
 
+
 class Method:
     Mask = 0
     StyleSheet = 1
 
+
 class RoundButton(QtWidgets.QPushButton):
     """
-    A nice rounded button. Currently set through fixed size only.
+    A nice rounded button. Two methods to rendering it out currently.
+    You can use this like a QPushButton
 
+    Mask:
+    Mask will cut the button into a circle. It also allows for custom stylesheets.
+    The downside though is that it is pixelated when drawing it out.
+    .. code-block:: python
+        roundBtn = RoundButton(self, "Hello World", method=Method.Mask)
+        roundBtn.setMethod(Method.Mask)  # Use this if you want to set it after
+
+    Stylesheet:
+    Style sheet creates a smooth circle button, no pixelation. However for rectangle buttons,
+    it wont be round and the user wont be able to use their own stylesheet.
+    .. code-block:: python
+        roundBtn = RoundButton(self, "Hello World", method=Method.StyleSheet)
+        roundBtn.setMethod(Method.StyleSheet)  # Use this if you want to set it after
+        roundBtn.setFixedSize(QtCore.QSize(24,24))  # Square dimensions recommended
 
     """
 
@@ -16,12 +33,25 @@ class RoundButton(QtWidgets.QPushButton):
         self.method = method
 
     def setMethod(self, method=Method.Mask):
+        """Set the method of rendering, Method.Mask or Method.StyleSheet
+
+        StyleSheet:
+            - Can't have own stylesheet
+            - Smooth Rendering
+
+        Mask:
+            - Pixelated edges
+            - Can set custom stylesheet
+
+        :param method: Method.Mask or Method.StyleSheet
+        :type method: int
+        :return:
+        """
         self.method = method
 
     def resizeEvent(self, event):
-        """
+        """Resize and update based on the method
 
-        :param size:
         :return:
         """
 
