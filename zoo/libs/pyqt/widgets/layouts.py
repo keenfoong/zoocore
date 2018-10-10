08a341625f7ecdc0aee5e12b426f835685372001
keenfoong <31301148+keenfoong@users.noreply.github.com>
2018-10-10 18:03:31 +1100

    Hotkey Edit widget  (#35)
    * A hotkey edit widget which detects which hotkey is currently being pressed. Required for the Hotkey editor
    
    * Documentation

--- zoo/libs/pyqt/widgets/layouts.py
+++ zoo/libs/pyqt/widgets/layouts.py
@@ -532,3 +532,114 @@ class CollapsableFrameLayout(QtWidgets.QWidget):
         """
         self.iconButton.clicked.connect(self.showHideWidget)
         self.titleFrame.mouseReleased.connect(self.showHideWidget)
+
+
+class HotkeyDetectEdit(QtWidgets.QLineEdit):
+    hotkeyEdited = QtCore.Signal()
+
+    def __init__(self, text="", parent=None, prefix="", suffix=""):
+        """ A line Edit which detects key combinations are being pressed(for hotkeys for instance).
+
+        Usage: It works similarly to a QLineEdit. Type in a hotkey combination to show the hotkey.
+
+        Example::
+
+            # eg Type in Ctrl+Alt+P and it would set the text of the QLineEdit to "Ctrl+Alt+P"
+            wgt = HotkeyDetectEdit()
+
+        :param text:
+        :param parent:
+        :param prefix:
+        :param suffix:
+        """
+        super(HotkeyDetectEdit, self).__init__(parent)
+
+        self.prefix = prefix
+        self.suffix = suffix
+
+        self.setText(text)
+
+        # str.maketrans('~!@#$%^&*()_+{}|:"<>?', '`1234567890-=[]\\;\',./')
+        self.specialkeys = {64: 50, 33: 49, 34: 39, 35: 51, 36: 52, 37: 53, 38: 55, 40: 57, 41: 48, 42: 56, 43: 61,
+                            63: 47, 60: 44, 126: 96, 62: 46, 58: 59, 123: 91, 124: 92, 125: 93, 94: 54, 95: 45}
+
+    def setText(self, text, resetCursor=True):
+        """ Set text of HotKeyEdit
+
+        :param text:
+        :param resetCursor:
+        :return:
+        """
+        text = self.prefix + text + self.suffix
+
+        super(HotkeyDetectEdit, self).setText(text)
+
+        if resetCursor:
+            self.setCursorPosition(0)
+
+    def keyPressEvent(self, e):
+        """ Update the text edit to whatever the hotkey is inputted
+
+        For example type in Ctrl+Alt+P and it would set the text of the QLineEdit to "Ctrl+Alt+P"
+
+        :param e:
+        :return:
+        """
+        keyStr = QtGui.QKeySequence(self.convertSpecChars(e.key())).toString()
+
+        # Return out if its only a modifier
+        if str(e.text()) == "":
+            return
+
+        self.text()
+        modifiers = QtWidgets.QApplication.keyboardModifiers()
+
+        # Feels like theres a better way to do this..
+        if modifiers == QtCore.Qt.ShiftModifier:
+            hotkey = 'Shift+' + keyStr
+        elif modifiers == QtCore.Qt.ControlModifier:
+            hotkey = 'Ctrl+' + keyStr
+        elif modifiers == QtCore.Qt.AltModifier:
+            hotkey = 'Alt+' + keyStr
+        elif modifiers == (QtCore.Qt.AltModifier | QtCore.Qt.ControlModifier):
+            hotkey = 'Ctrl+Alt+' + keyStr
+        elif modifiers == (QtCore.Qt.ControlModifier | QtCore.Qt.ShiftModifier):
+            hotkey = 'Ctrl+Shift+' + keyStr
+        elif modifiers == (QtCore.Qt.AltModifier | QtCore.Qt.ShiftModifier):
+            hotkey = 'Shift+Alt+' + keyStr
+        elif modifiers == (QtCore.Qt.AltModifier | QtCore.Qt.ControlModifier |
+                               QtCore.Qt.ShiftModifier):
+            hotkey = 'Ctrl+Alt+Shift+' + keyStr
+        elif modifiers == (QtCore.Qt.ControlModifier | QtCore.Qt.ShiftModifier):
+            hotkey = 'Ctrl+' + keyStr
+        else:
+            hotkey = keyStr
+
+        self.setText(hotkey)
+        self.hotkeyEdited.emit()
+
+    def convertSpecChars(self, charInt):
+        """ Converts special characters back to the original keyboard number
+
+        :param charInt:
+        :return:
+        """
+        if charInt in self.specialkeys:
+            return self.specialkeys[charInt]
+        return charInt
+
+
+class QHLine(QtWidgets.QFrame):
+    def __init__(self):
+        """ A nice horizontal line to space ui """
+        super(QHLine, self).__init__()
+        self.setFrameShape(QtWidgets.QFrame.HLine)
+        self.setFrameShadow(QtWidgets.QFrame.Sunken)
+
+
+class QVLine(QtWidgets.QFrame):
+    def __init__(self):
+        """ A nice vertical line to space ui """
+        super(QVLine, self).__init__()
+        self.setFrameShape(QtWidgets.QFrame.VLine)
+        self.setFrameShadow(QtWidgets.QFrame.Sunken)
