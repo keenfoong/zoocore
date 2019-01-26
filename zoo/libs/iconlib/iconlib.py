81fcd0402a9577ba2f0ea3b0ec355fae4bf0143b
keenfoong <31301148+keenfoong@users.noreply.github.com>
2019-01-26 18:58:58 +1100

    Lower case and minor code clean up tweaks (#82)

--- zoo/libs/iconlib/iconlib.py
+++ zoo/libs/iconlib/iconlib.py
@@ -131,7 +131,7 @@ class Icon(object):
 
     @classmethod
     def iconColorized(cls, iconName, size=16, color=(255, 255, 255), overlayName=None, overlayColor=(255,255,255)):
-        """Colorizes the icon from the library expects the default icon
+        """ Colorizes the icon from the library expects the default icon
         to be white for tinting.
 
         :param iconName: the icon name from the library
